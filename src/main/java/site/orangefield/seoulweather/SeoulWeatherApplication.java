37f5176d879869cd3a21b3634823aa3224a443d2
field <jsw_777@naver.com>
2022-06-14 10:13:35 +0900

    Initial Project

---
+++ src/main/java/site/orangefield/seoulweather/SeoulWeatherApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.seoulweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/orangefield/seoulweather/SeoulWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.seoulweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
